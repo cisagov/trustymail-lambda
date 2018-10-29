6b879bb2bc31d196acd66ce22303740f9b9994f7
Jeremy Frasier <jeremy.frasier@beta.dhs.gov>
2018-10-29 10:43:46 -0400

    Remove image building step
    Running docker-compose builds the image, so there is no need to build
    it explicitly.

--- travis_scripts/build_docker_image.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# semver uses a plus character for the build number (if present).
-# This is invalid for a Docker tag, so we replace it with a minus.
-version=$(./bump_version.sh show|sed "s/+/-/")
-docker build -t ${IMAGE_NAME}:$version .
