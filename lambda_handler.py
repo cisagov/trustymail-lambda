a64d4411d9d7d1e054d919ab646d267bd0c53064
Jeremy Frasier <jeremy.frasier@beta.dhs.gov>
2018-10-29 11:20:43 -0400

    Flesh out lambda_handler.py so it becomes something useful

--- lambda_handler.py
+++ lambda_handler.py
@@ -1,39 +1,9 @@
-# import json
+import json
 import logging
 
 import dns
 
-# from boto3 import client as boto3_client
-
-from trustymail.domain import Domain
-from trustymail.trustymail import mx_scan
-
-# This Lambda function expects the following environment variables to be
-# defined:
-# 1. queue_url - The url of the SQS queue containing the events to be
-# processed
-# 2. elasticsearch_url - A URL corresponding to an AWS Elasticsearch
-# instance, including the index where the DMARC aggregate reports
-# should be written
-# 3. elasticsearch_region - The AWS region where the Elasticsearch
-# instance is located
-
-# The file where any domains identified from the DMARC aggregate reports should
-# be saved.
-DOMAINS = None
-
-# The file where DMARC aggregate reports should be saved.
-REPORTS = None
-
-# Whether or not to delete the objects from the S3 bucket once they are
-# successfully processed
-DELETE = True
-
-# The XSD file against which the DMARC aggregate reports are to be be verified
-SCHEMA = 'dmarc/rua_mod.xsd'
-
-# The Dmarcian API token
-TOKEN = None
+from trustymail.trustymail import scan
 
 # In the case of AWS Lambda, the root logger is used BEFORE our Lambda handler
 # runs, and this creates a default handler that goes to the console.  Once
@@ -53,15 +23,20 @@ if root.handlers:
         root.removeHandler(handler)
 
         # Set up logging
-        log_level = logging.WARNING
+        log_level = logging.DEBUG
         logging.basicConfig(format='%(asctime)-15s %(levelname)s %(message)s',
                             level=log_level)
 
-# Boto3 clients for SQS, S3, and Lambda.  We make these static so they can be
-# reused if the Lambda function is called again on the same host.
-# sqs_client = boto3_client('sqs')
-# s3_client = boto3_client('s3')
-# lambda_client = boto3_client('lambda')
+# The default ports to check when scanning for SMTP servers
+DEFAULT_SMTP_PORTS = '25,465,587'
+
+# The default scan types to run
+DEFAULT_SCAN_TYPES = {
+    'mx': True,
+    'starttls': True,
+    'spf': True,
+    'dmarc': True
+}
 
 
 def handler(event, context):
@@ -69,45 +44,24 @@ def handler(event, context):
     Handler for all Lambda events
     """
     logging.info('AWS Event was: {}'.format(event))
+    logging.debug('AWS Context was: {}'.format(context))
 
     # Extract some variables from the event dictionary
-    # body = json.loads(event['Body'])
-    # domain = body['domain']
+    body = json.loads(event['Body'])
+    domain_name = body['domain_name']
+    timeout = int(body.get('timeout', 5))
+    smtp_timeout = int(body.get('smtp_timeout', 5))
+    smtp_localhost = body.get('smtp_localhost', None)
+    smtp_ports =  {int(port) for port in
+                   body.get('smtp_ports', DEFAULT_SMTP_PORTS).split(',')}
+    no_smtp_cache = bool(body.get('no_smtp_cache', False))
+    scan_types = body.get('scan_types', DEFAULT_SCAN_TYPES)
+    dns_hostnames = body.get('dns_hostnames', None)
+    if dns_hostnames is not None:
+        dns_hostnames = dns_hostnames.split(',')
 
-    #
     # Perform the scan
-    #
+    domain = scan(domain_name, timeout, smtp_timeout, smtp_localhost,
+                  smtp_ports, not no_smtp_cache, scan_types, dns_hostnames)
 
-    # Our resolver
-    #
-    # Note that it uses the system configuration in /etc/resolv.conf
-    # if no DNS hostnames are specified.
-    resolver = dns.resolver.Resolver(configure=False)
-    # This is a setting that controls whether we retry DNS servers if
-    # we receive a SERVFAIL response from them.  We set this to False
-    # because, unless the reason for the SERVFAIL is truly temporary
-    # and resolves before trustymail finishes scanning the domain,
-    # this can obscure the potentially informative SERVFAIL error as a
-    # DNS timeout because of the way dns.resolver.query() is written.
-    # See
-    # http://www.dnspython.org/docs/1.14.0/dns.resolver-pysrc.html#Resolver.query.
-    resolver.retry_servfail = False
-    # Set some timeouts.  The timeout should be less than or equal to
-    # the lifetime, but longer than the time a DNS server takes to
-    # return a SERVFAIL (since otherwise it's possible to get a DNS
-    # timeout when you should be getting a SERVFAIL.)  See
-    # http://www.dnspython.org/docs/1.14.0/dns.resolver-pysrc.html#Resolver.query
-    # and
-    # http://www.dnspython.org/docs/1.14.0/dns.resolver-pysrc.html#Resolver._compute_timeout.
-    resolver.timeout = float(30)
-    resolver.lifetime = float(30)
-    # If the user passed in DNS hostnames to query against then use them
-    resolver.nameservers = [
-        '8.8.8.8',
-        '8.8.4.4'
-    ]
-    domain = Domain('dhs.gov', 30, 30, None, {25}, True,
-                    ['8.8.8.8', '8.8.4.4'])
-    mx_scan(resolver, domain)
-
-    # logging.debug('Response from do_it() is {}'.format(returnVal))
+    logging.debug('Response from scan() is {}'.format(domain))
