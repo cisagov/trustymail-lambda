7e97d49974b5b0f49ad242529813778f42b3f24f
Jeremy Frasier <jeremy.frasier@beta.dhs.gov>
2018-10-29 14:23:44 -0400

    Bugfixes
    * There is no "body" element in the event, just the event itself
    * Import trustymail later, so we can monkey-patch it to set the public
      suffix list location and make it read-only

--- lambda_handler.py
+++ lambda_handler.py
@@ -1,8 +1,6 @@
 import json
 import logging
 
-from trustymail.trustymail import scan
-
 # In the case of AWS Lambda, the root logger is used BEFORE our Lambda handler
 # runs, and this creates a default handler that goes to the console.  Once
 # logging has been configured, calling logging.basicConfig() has no effect.  We
@@ -42,23 +40,27 @@ def handler(event, context):
     Handler for all Lambda events
     """
     logging.info('AWS Event was: {}'.format(event))
-    logging.debug('AWS Context was: {}'.format(context))
 
     # Extract some variables from the event dictionary
-    body = json.loads(event['Body'])
-    domain_name = body['domain_name']
-    timeout = int(body.get('timeout', 5))
-    smtp_timeout = int(body.get('smtp_timeout', 5))
-    smtp_localhost = body.get('smtp_localhost', None)
+    domain_name = event['domain_name']
+    timeout = int(event.get('timeout', 30))
+    smtp_timeout = int(event.get('smtp_timeout', 5))
+    smtp_localhost = event.get('smtp_localhost', None)
     smtp_ports = {int(port) for port in
-                  body.get('smtp_ports', DEFAULT_SMTP_PORTS).split(',')}
-    no_smtp_cache = bool(body.get('no_smtp_cache', False))
-    scan_types = body.get('scan_types', DEFAULT_SCAN_TYPES)
-    dns_hostnames = body.get('dns_hostnames', None)
+                  event.get('smtp_ports', DEFAULT_SMTP_PORTS).split(',')}
+    no_smtp_cache = bool(event.get('no_smtp_cache', False))
+    scan_types = event.get('scan_types', DEFAULT_SCAN_TYPES)
+    dns_hostnames = event.get('dns_hostnames', None)
     if dns_hostnames is not None:
         dns_hostnames = dns_hostnames.split(',')
 
     # Perform the scan
+    import trustymail
+    # Monkey patching trustymail to make it cache the PSL where we
+    # want, and to make the PSL cache read-only.
+    trustymail.PublicSuffixListFilename = 'cache/public-suffix-list.txt'
+    trustymail.PublicSuffixListReadOnly = True
+    from trustymail.trustymail import scan
     domain = scan(domain_name, timeout, smtp_timeout, smtp_localhost,
                   smtp_ports, not no_smtp_cache, scan_types, dns_hostnames)
 
